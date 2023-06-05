cfc0db764dd8d10e2fc18b27954c37941c380fde
gguio <gdguio@outlook.com>
2023-06-05 16:20:16 -0400

    Unit Test ModelConvert for Query

--- StockSageServiceLambda/src/test/java/stocksageservice/models/QueryModelTest.java
+++
@@ -1,7 +0,0 @@
-package stocksageservice.models;
-
-public class QueryModelTest {
-
-    //When the QueryModel is called then return the query model based on a model input.
-    //Mock all inputs and internal methods.
-}
