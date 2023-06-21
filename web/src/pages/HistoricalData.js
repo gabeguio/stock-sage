a6cc604312ac742a0b12e5659584b2ac371735b3
gguio <gdguio@outlook.com>
2023-06-21 15:14:09 -0400

    Form Bar almost done

--- web/src/pages/HistoricalData.js
+++ web/src/pages/HistoricalData.js
@@ -9,14 +9,14 @@ import DataStore from "../util/DataStore";
 class HistoricalData extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'createQuery', 'createStocksTable', 'setStatusBar'], this);
+        this.bindClassMethods(['mount', 'retrieveQueryResultsAndAddToPage', 'updateStocksTable', 'setStatusBar'], this);
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
         console.log("HistoricalData constructor");
     }
 
     mount() {
-        document.getElementById('loadStocks').addEventListener('click', this.createQuery);
+        document.getElementById("form-submit-btn").addEventListener("click", this.retrieveQueryResultsAndAddToPage);
 
         this.header.addHeaderToPage();
         this.client = new StockSageClient();
@@ -37,66 +37,65 @@ class HistoricalData extends BindingClass {
         }
     }
 
-    async createQuery(event) {
-        // prevent page refresh
-        event.preventDefault();
+    async retrieveQueryResultsAndAddToPage(event) {
+        // // prevent page refresh
+        // event.preventDefault();
 
         // button set to 'Loading...' when clicked
-        const button = document.getElementById('loadStocks');
+        const button = document.getElementById('form-submit-btn');
         button.innerText = 'Loading...';
 
         // get username and fields for createQuery request
         const username = (await this.client.authenticator.getCurrentUserInfo()).email
-        const startDate = document.getElementById('startDateInput').value;
-        const endDate = document.getElementById('endDateInput').value;
-        const frequency = document.getElementById('frequencyInput').value;
-        const symbol = document.getElementById('symbolInput').value;
+        console.log(username);
+        var startDate = document.getElementById("start-date").value;
+        console.log(startDate);
+        var endDate = document.getElementById("end-date").value;
+        console.log(endDate);
+        var aggregation = document.getElementById("aggregation-period").value;
+        console.log(aggregation);
+        var stockSymbol = document.getElementById("stock-symbol").value;
+        console.log(stockSymbol);
     
 
         // submit fields to createQuery api
-        const stockList = await this.client.createQuery(username, startDate, endDate, frequency, symbol);
+        const stockList = await this.client.createQuery(username, startDate, endDate, aggregation, stockSymbol);
 
         // store stockList in dataStore
         this.dataStore.set('stockList', stockList);
 
         //button text reverted
-        button.innerHTML = "Load Stocks"
+        button.innerHTML = "Apply";
 
         // render stocks table and add to page
-        document.getElementById('viewStocksTable').innerHTML = this.createStocksTable(stockList);
+        this.updateStocksTable(stockList);
     }
 
-    createStocksTable(stocks) {
-        if (stocks === 0) {
-            return `<p>No Stocks Found</p>`
-        }
-        
-        let html = `
-            <table>
-                <tr>
-                    <th>Date</th>
-                    <th>Open</th>
-                    <th>High</th>
-                    <th>Low</th>
-                    <th>Close</th>
-                    <th>Volume</th>
-                </tr>`;
+    async updateStocksTable(stocks) {
+        // Get the table body element
+        var tableBody = document.querySelector("#stock-table tbody");
 
-        for (const stock of stocks) {
-            html += `
-                <tr>
-                    <td>${stock.date}</td>
-                    <td>${stock.open}</td>
-                    <td>${stock.high}</td>
-                    <td>${stock.low}</td>
-                    <td>${stock.close}</td>
-                    <td>${stock.volume}</td>
-                </tr>`
-        }
+        // Loop through the stockDataList and create table rows
+        for (var i = 0; i < stocks.length; i++) {
+        var stock = stocks[i];
+        var newRow = tableBody.insertRow();
 
-        html += `</table>`
-        return html;
+        // Create table cells for each property in the object
+        var cell1 = newRow.insertCell();
+        cell1.textContent = stock.date;
+        var cell2 = newRow.insertCell();
+        cell2.textContent = Number(stock.open).toFixed(2);
+        var cell3 = newRow.insertCell();
+        cell3.textContent = Number(stock.high).toFixed(2);
+        var cell4 = newRow.insertCell();
+        cell4.textContent = Number(stock.low).toFixed(2);
+        var cell5 = newRow.insertCell();
+        cell5.textContent = Number(stock.close).toFixed(2);
+        var cell6 = newRow.insertCell();
+        cell6.textContent = Number(stock.volume).toLocaleString();
     }
+}
+
 }
 
 const main = async () => {
