77b369ed092ba7d7d464f3e5ce69e6826afdcc1f
gguio <gdguio@outlook.com>
2023-06-21 21:02:11 -0400

    Delete button works on Saved Requests

--- web/src/pages/SavedQueries.js
+++ web/src/pages/SavedQueries.js
@@ -6,15 +6,13 @@ import DataStore from "../util/DataStore";
 class SavedQueries extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['mount', 'loadSavedQueries', 'unsaveQuery', 'submitEditQuery', 'setStatusBar'], this);
+        this.bindClassMethods(['mount', 'setStatusBar', 'loadSavedQueries', 'renderSavedRequests', 'createSavedRequestCard', 'deleteSavedRequest', 'editSavedRequest'], this);
         this.dataStore = new DataStore;
         this.header = new Header(this.dataStore);
         console.log("SavedQueries constructor")
     }
 
     mount () {
-        document.getElementById('loadSavedQueries').addEventListener('click', this.loadSavedQueries);
-        document.getElementById('submitEditQuery').addEventListener('click', this.submitEditQuery);
         this.header.addHeaderToPage();
         this.client = new StockSageClient();
     }
@@ -34,110 +32,113 @@ class SavedQueries extends BindingClass {
         }
     }
 
-
-    async loadSavedQueries(event) {
-        // prevent page refresh
-        event.preventDefault();
-
-        // button set to 'Loading...' when clicked
-        const loadButton = document.getElementById('loadSavedQueries');
-        loadButton.innerText = 'Loading...';
-
+    async loadSavedQueries() {
         //get username for Saved queries
         const username = (await this.client.authenticator.getCurrentUserInfo()).email
 
         // API request/response
         const SavedQueriesList = await this.client.getSavedQueriesByUsername(username);
 
+        // Sort the list in descending order before storing
+        SavedQueriesList.sort(function(a, b) {
+            return b.queryId.localeCompare(a.queryId);
+        });
+
         // store SavedQueriesList in dataStore
         this.dataStore.set('SavedQueriesList', SavedQueriesList);
-
-        //reset button text
-        loadButton.innerHTML = "Load Saved Queries";
-
-        // retrieve list of Saved queries, iterate through the list adding each object to an <ol>
-        var olElement = document.getElementById("viewSavedQueriesList");
 
         // retrieve queries from the datastore
         var queries = this.dataStore.get('SavedQueriesList');
         
         // sort queries in descending order by queryId
-        queries.sort(function(a, b) {
-            return b.queryId.localeCompare(a.queryId);
-        });
 
-        // iterate through each item in the savedQueries response
-        for (var i = 0; i < queries.length; i++) {
-            // create an element for each item in the ordered list
-            var listItem = document.createElement("li");
-
-            // create a delete button for each item on the list
-            var deleteButton = document.createElement("button");
-
-            // name each button 'Delete'
-            deleteButton.textContent = 'Delete';
-
-            // create an edit button for each item on the list
-            var editButton = document.createElement("button");
-
-            // name each button 'Edit'
-            editButton.textContent = 'Edit';
-
-            // store query id on each delete button
-            deleteButton.setAttribute('data-query-id', queries[i].queryId);
-            editButton.setAttribute('data-query-id', queries[i].queryId);
-
-            // add event listeners for each delete button
-            deleteButton.addEventListener('click', this.unsaveQuery);
-            deleteButton.addEventListener('click', this.editQuery);
-
-            // populate each item in the list with each query attribute
-            listItem.textContent = queries[i].startDate
-            + ", " + queries[i].endDate
-            + ", " + queries[i].frequency
-            + ", " + queries[i].symbol
-            + ", " + queries[i].title
-            + ", " + queries[i].content
-
-            // add the delete button node to the list item
-            listItem.appendChild(deleteButton);
-
-            // add the list item node to the ordered list
-            olElement.appendChild(listItem);
-          }
+        this.renderSavedRequests();
     }
 
-    async unsaveQuery(event) {
-        // Grab queryId off the button that is being clicked
-        const username = (await this.client.authenticator.getCurrentUserInfo()).email;
-        const queryId = event.target.getAttribute('data-query-id');
+      renderSavedRequests() {
+        const savedRequests = this.dataStore.get('SavedQueriesList');
+        const savedRequestList = document.getElementById('saved-request-list');
+        savedRequestList.innerHTML = '';
+      
+        for (const savedRequest of savedRequests) {
+          const savedRequestCard = this.createSavedRequestCard(savedRequest);
+          savedRequestList.appendChild(savedRequestCard);
+        }
+      }
 
-        // Submit request with username and query id
-        await this.client.unsaveQuery(username, queryId);
+      createSavedRequestCard(savedRequest) {
+        const savedRequestCard = document.createElement('div');
+        savedRequestCard.classList.add('object-card');
+      
+        const title = document.createElement('h3');
+        title.textContent = savedRequest.title;
+      
+        const startDate = document.createElement('p');
+        startDate.textContent = `Start Date: ${savedRequest.startDate}`;
+      
+        const endDate = document.createElement('p');
+        endDate.textContent = `End Date: ${savedRequest.endDate}`;
+      
+        const symbol = document.createElement('p');
+        symbol.textContent = `Symbol: ${savedRequest.symbol}`;
+      
+        const frequency = document.createElement('p');
+        frequency.textContent = `Frequency: ${savedRequest.frequency}`;
+      
+        const content = document.createElement('p');
+        content.textContent = savedRequest.content;
+      
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', this.editSavedRequest);
+        editButton.setAttribute('queryId', savedRequest.queryId)
+      
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', this.deleteSavedRequest);
+        deleteButton.setAttribute('queryId', savedRequest.queryId)
+      
+        savedRequestCard.appendChild(title);
+        savedRequestCard.appendChild(startDate);
+        savedRequestCard.appendChild(endDate);
+        savedRequestCard.appendChild(symbol);
+        savedRequestCard.appendChild(frequency);
+        savedRequestCard.appendChild(content);
+        savedRequestCard.appendChild(editButton);
+        savedRequestCard.appendChild(deleteButton);
+      
+        return savedRequestCard;
+      }
 
-        // Notify the user that the query has been unsaved
-        alert("Query has been unsaved from your list.");
-    }
-
-    async submitEditQuery(event) {
-        event.preventDefault();
-        console.log("starting submitEditQuery function");
+    async editSavedRequest(event) {
+        // event.preventDefault();
+        // console.log("starting submitEditQuery function");
+        console.log("editSaveRequest");
       
         // Get the form values
         const username = (await this.client.authenticator.getCurrentUserInfo()).email
-        const queryId = document.getElementById('queryId').value;
+        const queryId = event.target.getAttribute('queryId')
         const title = document.getElementById('title').value;
         const content = document.getElementById('content').value;
 
         // API request/response
         await this.client.updateTitleAndContent(username, queryId, title, content);
       }
+      
+      // Function to handle delete button click
+    async deleteSavedRequest(event) {
+        const username = (await this.client.authenticator.getCurrentUserInfo()).email
+        const queryId = event.target.getAttribute('queryId');
+        await this.client.unsaveQuery(username, queryId);
+        // renderSavedRequests();
+    }
 }
 
 const main = async () => {
     const savedQueries = new SavedQueries();
     savedQueries.mount();
     savedQueries.setStatusBar();
+    savedQueries.loadSavedQueries();
 };
 
 window.addEventListener('DOMContentLoaded', main);
